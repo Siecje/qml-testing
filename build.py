8c3af798233af2d614b2e1870d08dbff77599e34
Cody Scott <cody.j.b.scott@gmail.com>
2016-10-13 14:52:38 -0400

    Fixing osnap build

--- build.py
+++ build.py
@@ -4,9 +4,11 @@ import main
 APPLICATION_NAME = 'app'
 AUTHOR = 'test'
 
+PY_VERSION = '3.5.2'
+
 def make_installer(verbose):
-    osnap.installer.make_installer(AUTHOR, APPLICATION_NAME, 'this is my test example', 'www.mydomain.com',
-                                   [APPLICATION_NAME], '1.0.0', verbose=verbose)
+    osnap.installer.make_installer(PY_VERSION, APPLICATION_NAME, AUTHOR, 'this is my test example', 'www.mydomain.com',
+                                   compile_code=True)
 
 if __name__ == '__main__':
     make_installer(True)
